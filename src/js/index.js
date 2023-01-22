f03d0820c6c5545f60431b624b1670553259053d
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-01-22 19:11:37 +0100

    Update the index.js with the imported function (renderMenu)

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
 import menuArray from './data.js';
+import renderMenu from './renderMenu.js';
 
-/*
-  <div class="app__body-menu-item">
-    <div class="app__body-menu-item-info">
-      <img class="app__body-menu-item-img" src="" alt="pizza icon">
-      <div class="app__body-menu-item-details">
-        <h2 class="app__body-menu-item-name">Name</h2>
-        <p class="app__body-menu-item-desc">desc</p>
-        <p class="app__body-menu-item-price">price</p>
-      </div>
-    </div>
-    <button class="app__body-menu-item-btn" aria-label='add name to cart at price dollar per unit'>
-      +
-    </button>
-  </div>
-*/
+// Render the menu
+renderMenu(menuArray);
