561158e316e49d89d6bebcddd8023d494fbdb411
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-01-23 21:49:33 +0100

    Update the removeorder

--- src/js/removeOrder.js
+++ src/js/removeOrder.js
@@ -3,7 +3,6 @@ import renderOrders from './renderOrders.js';
 const removeOrder = (itemId) => {
   const orderItems = JSON.parse(localStorage.getItem('order'));
   const remainingItems = orderItems.filter((item, index) => index !== itemId);
-  console.log(remainingItems);
   localStorage.setItem('order', JSON.stringify(remainingItems));
   renderOrders();
 };
