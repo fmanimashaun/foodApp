ff7f5620c6ec02d320125acbd16b3148ed456c58
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-01-23 12:30:32 +0100

    Update build

--- order.js
+++
@@ -1,8 +0,0 @@
-const addOrder = (order) => {
-  return `
-  <div class="app__footer-order-item">
-    <p class="app__footer-order-item-name">${order.name}<button class="app__footer-order-item-btn">remove</button></p>
-    <p class="app__footer-order-item-price">$${order.price}</p>
-  </div>
-  `;
-};
